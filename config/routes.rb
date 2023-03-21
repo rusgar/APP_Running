e86d312ce8aeffc69abdd6b2a469f1af1826ef05
Edu Rus <edamamon@gmail.com>
2023-03-21 09:39:16 +0100

    Primeras rutas

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/products', to:'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 
 end
