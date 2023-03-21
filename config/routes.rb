5065e2d210233c4288dc18678c2addf6d6f5b262
Edu Rus <edamamon@gmail.com>
2023-03-21 10:08:56 +0100

    Primer Formulario

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/products/new', to: 'products#new', as: :new_product
   get '/products', to:'products#index'
   get '/products/:id', to: 'products#show', as: :product
 
