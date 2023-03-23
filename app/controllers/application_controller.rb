58701d511d54df934823ac8e9897a4a5bc80ec0e
Edu Rus <edamamon@gmail.com>
2023-03-23 10:27:11 +0100

    Productos asociados a clientes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Authorization
   include Language
-  include Pagy::Backend      
+  include Pagy::Backend
+  include Error     
 
 end
