553871880dfc89eadb6c538cc10ca1e9e9325926
Edu Rus <edamamon@gmail.com>
2023-03-24 11:02:37 +0100

    menus con cierres de js

---
+++ app/javascript/controllers/menu_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets= ["items"]  
+  open() {
+    this.itemsTarget.classList.remove('translate-x-full')
+  }
+  close () {
+    this.itemsTarget.classList.add('translate-x-full')
+  }
+
+}
